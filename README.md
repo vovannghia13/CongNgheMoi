2f2a481bb48cd5da92534aabd330680086026bda
luuquangduc7cd <luuquangduc7cd@gmail.com>
2019-12-09 12:06:13 +0700

    Optimize

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # CongNgheMoi
+
+#Python requirements
+Version: 3.x.x
+Libraries: flask_socketio, flask, random, time, threading, PySerial
+
+#Arduino requirements
+Software: Arduino IDE
+Hardware: Arduino Uno R3
+Libraries: Servo.h, DHT_U.h, DHT.h
--- Web/main.py
+++ Web/main.py
@@ -4,14 +4,15 @@ from random import random
 from time import sleep
 from threading import Thread, Event
 import congnghemoi as n8
-import serial
+"""import serial
+
 
 ser = serial.Serial()
 ser.port = "/dev/ttyACM0"
 ser.baudrate = 9600
 ser.close()
 ser.open()
-
+"""
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 app.config['DEBUG'] = True
@@ -24,19 +25,24 @@ thread = Thread()
 thread_stop_event = Event()
 
 
-def getData():
+"""def getData():
     while True:
         if ser.isOpen():
-            data = ser.readlone()
+            data = ser.readline()
             data = data.split("*")
             temperature = data[0]
             humidity = data[1]
-        else
+        else:
             temperature = round(random()*10, 3)
             humidity = round(random()*100, 3)
         socketio.emit('newdata', {'temperature': temperature, 'humidity': humidity}, namespace='/temperature')
+        socketio.sleep(1)"""
+def getData():
+    while True:
+        temperature = 26 + random() % 4
+        humidity = 50 + random() % 20
+        socketio.emit('newdata', {'temperature': temperature, 'humidity': humidity}, namespace='/temperature')
         socketio.sleep(1)
-
 
 @app.route("/")
 def home():
